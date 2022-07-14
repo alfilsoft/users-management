436dcbdeb6273bcbfd58ca39a1d53e1ac77e6581
unknown <aponte10anthony@gmail.com>
2022-07-13 20:31:51 -0500

    U01 create footer and header

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import './css/Footer.css';
+
+function Footer() {
+    return (
+      <div className="Footer">
+        <footer className="Footer-Footer">
+          
+          &copy;  CRUD Application /  phone:3555555 / Crud@hotmail.com / Business CRUD  
+          
+        </footer>
+      </div>
+    );
+  }
+  
+  export default Footer;
