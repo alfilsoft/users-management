40a7412626fed9fd64f55f809da424a5f927a332
anthonyaponte <104374755+anthonyaponte@users.noreply.github.com>
2022-07-13 20:43:22 -0500

    U01 create footer and header (#1)

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import './css/header.css';
+
+function header() {
+  return (
+    <div className="header">
+      <header className="App-header">
+        <h1> Crude Application</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default header;
